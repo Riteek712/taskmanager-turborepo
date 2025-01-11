281d5505030dd8e84d327f20c5fd3662ab29d2fa
Riteek Rakesh <riteek7.12@gmail.com>
2025-01-11 19:37:34 +0530

    setup types package

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // If you need to send cookies with the request
+  });
+  app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+  .setTitle('Task Manager API Documentation')
+  .setDescription("A rest api to list user's todos/tasks, authorization implemented using JWT token. THe API is made using NestJs.")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('/swagger', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+});
+  await app.listen(3001);
 }
 bootstrap();
+ 

64999f38c2e7917f7f2055bab9f4ddea316a809b
Riteek Rakesh <riteek7.12@gmail.com>
2025-01-12 14:38:12 +0530

    CHORE: Developed Login page

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -1,9 +1,11 @@
+import { LoginFormDemo } from "@/components/ui/login-form";
+
 const LogInPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
       <div className="bg-black p-8 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center">
-        <h1 className="text-center text-2xl font-bold mb-4">Sign up page</h1>
-        {/* <SignupFormDemo /> */}
+        <h1 className="text-center text-2xl font-bold mb-4">Log in page</h1>
+        <LoginFormDemo />
       </div>
     </div>
   );
